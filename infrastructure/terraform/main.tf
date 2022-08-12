ff12fc75c2cb53b839156682bd823b15b6df6699
Mymaks <mymaks51@gmail.com>
2022-08-12 20:06:29 +0300

    Added EKS cluster, not fully wokring

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -2,22 +2,20 @@ provider "aws" {
   region = local.region
 }
 
-data "aws_eks_cluster" "default" {
-  name = local.name
-}
-
-data "aws_eks_cluster_auth" "default" {
-  name = local.name
-}
-
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.default.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.default.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.default.token
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    # This requires the awscli to be installed locally where Terraform is executed
+    args = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
+  }
 }
 
 locals {
-  name            = "final-task"
+  name            = "ex-${replace(basename(path.cwd), "_", "-")}"
   cluster_version = "1.22"
   region          = "eu-west-1"
 
